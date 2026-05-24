c75786f8a06565d1835a5ca792823bf00b633ce6
Bluffer1997 <40371711+Bluffer1997@users.noreply.github.com>
2026-05-23 21:01:53 -0700

    js

--- 2026/hotel/script.js
+++ 2026/hotel/script.js
@@ -1,3 +1,20 @@
 const loginBtn = document.querySelector(".login-btn");
-
+const signupBtn = document.querySelector(".signup-btn");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-Content");
 console.log(loginBtn);
+console.log(signupBtn);
+console.log(modal);
+console.log(modalContent);
+
+loginBtn.addEventListener("click", () => {
+   modal.style.display = "flex"; 
+});
+
+modal.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+.modalContent.addEventListener("click", (e) => {
+    e.stopPropogation();
+})
