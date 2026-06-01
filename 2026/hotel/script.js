dff6b1eff9790303356eec576d15c1169d45e647
Bluffer1997 <40371711+Bluffer1997@users.noreply.github.com>
2026-05-31 21:57:29 -0700

    Update script.js

--- 2026/hotel/script.js
+++ 2026/hotel/script.js
@@ -1,19 +1,20 @@
 //dates
-const checkIn = document.querySelector("#checkIn");
-const checkOut = document.querySelector("#checkOut");
-const checkInValue = document.getElementById("checkIn").value;
-const checkOutValue = document.getElementById("checkOut").value;
 const searchButton = document.querySelector("#searchDate");
 searchButton.addEventListener("click", () => {
-    alert(checkInValue);
-    alert(checkOutValue);
+    const checkInValue = document.getElementById("checkIn").value;
+    const checkOutValue = document.getElementById("checkOut").value;
+    const CheckInDate = new Date(checkInValue);
+    const checkOutDate = new Date(checkOutValue);
+    const days = (checkOutDate - CheckInDate) / (1000 * 60 * 60 * 24);
+    alert('You will check in on ' + checkInValue);
+    console.log('Checking in: ' + checkInValue);
+    alert('You will check out on ' + checkOutValue);
+    console.log('Checking out: ' + checkOutValue);
+    alert('You will rent this room for ' + days + ' days.');
+    console.log('Renting for ' + days + ' days.');
+
 })
 
-//console.log(checkIn);
-//console.log(checkOut);
-//console.log(searchButton);
-console.log(checkInValue)
-console.log(checkOutValue)
 
 // modal 
 const loginBtn = document.querySelector(".login-btn");
